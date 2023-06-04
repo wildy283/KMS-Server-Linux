edee56cca97c2a74692772b9d306b34821e52187
Wildy Sheverando <135487348+wildy238@users.noreply.github.com>
2023-06-04 12:47:11 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# https://github.com/wildy238/KMS-Server-Linux
+
+cp vlmcsd /usr/bin/vlmcsd
+chmod +x /usr/bin/vlmcsd
+
+cp kms-server.service /etc/systemd/system/
+systemctl enable kms-server
+systemctl start kms-server
+
+echo "Success"
